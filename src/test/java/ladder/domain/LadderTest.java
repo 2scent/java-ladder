adaae49c810f3ed8557b9e2924599ace56ba4da0
ad7549 <ad7549@gmail.com>
2021-04-11 18:25:36 +0900

    feat: Add Ladder

---
+++ src/test/java/ladder/domain/LadderTest.java
@@ -0,0 +1,22 @@
+package ladder.domain;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LadderTest {
+    @Test
+    public void create() {
+        final List<LadderLine> ladderLines = Arrays.asList(
+                new LadderLine(Arrays.asList(true, true, false, true, true)),
+                new LadderLine(Arrays.asList(true, true, false, true, true)),
+                new LadderLine(Arrays.asList(true, true, false, true, true)),
+                new LadderLine(Arrays.asList(true, true, false, true, true))
+        );
+
+        assertThat(new Ladder(ladderLines)).isEqualTo(new Ladder(ladderLines));
+    }
+}
